5d9d1902b76389b2b01df09d270393590564c056
chalolowa <locha.softwaredev@gmail.com>
2025-06-11 14:42:09 +0300

    On branch development
     Changed the entire design architecture for the dapp
     -identity-broker canister will handle authentication with hybrid approach(Internet Identity + Google sign-in).
     -wallet-factory canister will handle wallet management with ckbtc/ckusdc vaults.
     -fund-core canister will handle acturial models, compliance and aml checks.
     -token-factory canister will handle staking and tokens management.
     -watchdog canister will act as security monitor.

--- token-registry/src/token-registry-backend/src/token_types.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Debug, CandidType, Serialize, Deserialize)]
-pub struct TokenMetadata {
-    pub symbol: String,
-    pub name: String,
-    pub decimals: u8,
-    pub ledger_canister: Principal,
-    pub is_active: bool,
-}
